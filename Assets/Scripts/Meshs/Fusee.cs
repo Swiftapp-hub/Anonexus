93cba147ccd7ee7d5d87af169982edc28848cdbb
SwiftApp <swiftyassistant.io@gmail.com>
2022-12-09 20:14:33 +0100

    Finishing first timeline

--- Assets/Scripts/Meshs/Fusee.cs
+++ Assets/Scripts/Meshs/Fusee.cs
@@ -3,10 +3,11 @@ using UnityEngine;
 public class Fusee : MonoBehaviour
 {
     public int speed = 30;
+    public int speedRotate = 10;
 
-    // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * (Time.deltaTime * speed));
+        transform.Rotate(Vector3.forward * (Time.deltaTime * speedRotate));
     }
 }
