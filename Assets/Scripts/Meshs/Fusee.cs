7b45885411b5ea08b109470163b48695ac579da4
SwiftApp <swiftyassistant.io@gmail.com>
2022-12-09 15:02:43 +0100

    Change to Rider IDEA and add first virtual cam

--- Assets/Scripts/Meshs/Fusee.cs
+++ Assets/Scripts/Meshs/Fusee.cs
@@ -1,18 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Fusee : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int speed = 30;
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
     }
 }
